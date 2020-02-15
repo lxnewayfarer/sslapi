1017f8a650512ab51f819aad37c0e01fb9330700
lxnewayfarer <lxnewayfarer@yandex.ru>
2020-02-16 02:16:29 +0700

    ff

--- app/controllers/api/domain_controller.rb
+++ app/controllers/api/domain_controller.rb
@@ -1,8 +1,17 @@
 module Api
     class DomainController < ApplicationController
-        def index
-            domains = Domain.order('created_at DESC');
-            render json: {status: 'SUCCESS', message: 'done', data: domains}, status: :ok   
+        def create
+            domain = Domain.new(domain_name);
+            if domain.save
+                render json: {status: 'SUCCESS', data: domain}, status: :ok   
+            else
+                render json: {status: 'ERROR', data: domain.errors}, status: :ok   
+            end
+        end
+
+        private
+        def domain_name
+            params.permit(:domain)
         end
     end
 end
